22345de22b7f176813accf7a4438be6a1a792c61
Andrew Potter <agpotter@gmail.com>
2022-06-15 20:00:13 -0700

    Add missing files

---
+++ src/part2.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <array>
+#include <string_view>
+#include <optional>
+#include "volumes.h"
+
+namespace part_2
+{
+    using namespace std::string_view_literals;
+
+    constinit std::array vol_1 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_2 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_3 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_4 = std::to_array<volume_definition>({
+            {},
+        });
+
+}
---
+++ src/part4.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <array>
+#include <string_view>
+#include <optional>
+#include "volumes.h"
+
+namespace part_4
+{
+    using namespace std::string_view_literals;
+
+    constinit std::array vol_1 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_2 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_3 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_4 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_5 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_6 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_7 = std::to_array<volume_definition>({
+            {},
+        });
+    constinit std::array vol_8 = std::to_array<volume_definition>({
+            {},
+        });
+}
