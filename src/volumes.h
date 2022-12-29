aaed8847e08e69f4d29861737f07686b23db8e3e
Andrew Potter <agpotter@gmail.com>
2022-12-29 13:57:34 -0800

    Use ctre for chapter_type

--- src/volumes.h
+++ src/volumes.h
@@ -12,6 +12,7 @@
 #include <ranges>
 
 #include "outcome/result.hpp"
+#include "ctre.hpp"
 
 enum class volume
 {
@@ -103,6 +104,8 @@ std::ostream& operator<<(std::ostream& os, volume v);
 std::ostream& operator<<(std::ostream& os, omnibus v);
 std::ostream& operator<<(std::ostream& os, const std::variant<omnibus, volume>& v);
 
+
+
 struct volume_definition
 {
     volume vol;
@@ -110,6 +113,7 @@ struct volume_definition
     std::string_view mediatype;
     std::optional<std::string_view> toc_label;
     bool in_spine;
+    chapter_type type;
 
     void print_id(std::ostream &os) const {
         auto pos = href.find_last_of('/');
@@ -127,17 +131,128 @@ struct volume_definition
         return ss.str();
     }
 
-    chapter_type get_chapter_type() const;
+    constexpr chapter_type get_chapter_type() const { return type; };
 
-    inline friend bool operator==(const volume_definition& lhs, const volume_definition& rhs)
+    constexpr inline friend bool operator==(const volume_definition& lhs, const volume_definition& rhs)
     {
         return lhs.get_chapter_type() == rhs.get_chapter_type();
     }
-    inline friend std::strong_ordering operator<=>(const volume_definition& lhs, const volume_definition& rhs)
+    constexpr inline friend std::strong_ordering operator<=>(const volume_definition& lhs, const volume_definition& rhs)
     {
         return lhs.get_chapter_type() <=> rhs.get_chapter_type();
     }
+
+    constexpr chapter_type
+    evaluate_chapter_type() const {
+        constexpr auto cover_regex                     = ctre::search<"cover.xhtml">;
+        constexpr auto frontmatter_regex               = ctre::search<"text/front", ctre::case_insensitive>;
+        constexpr auto chapter_regex                   = ctre::search<"(xhtml/p-[0-9]*.xhtml|cover.xhtml|text/character|chapter|prologue|epilogue|x[0-9_]*.xhtml|text/[0-9]*.xhtml|text/insert|extra|side|temple)", ctre::case_insensitive>;
+        // ehrenfest city is frontmatter3.xhtml
+        constexpr auto map_ehrenfest_city_label_regex  = ctre::search<"Map of Ehrenfest", ctre::case_insensitive>; // can also use toc_label but this works for now
+        constexpr auto map_ehrenfest_duchy_label_regex = ctre::search<"Map of Ehrenfest Duchy", ctre::case_insensitive>; // can also use toc_label but this works for now
+        constexpr auto map_yurgenschmidt_label_regex   = ctre::search<"Map of Yurgenschmidt", ctre::case_insensitive>;
+        constexpr auto map_ehrenfest_href_regex        = ctre::search<"map.xhtml", ctre::case_insensitive>; // can also use toc_label but this works for now
+        constexpr auto map_yurgenschmidt_href_regex    = ctre::search<"map2.xhtml", ctre::case_insensitive>;
+        constexpr auto family_tree_regex               = ctre::search<"tree.xhtml", ctre::case_insensitive>;
+        constexpr auto afterword_regex                 = ctre::search<"afterword.xhtml", ctre::case_insensitive>;
+        constexpr auto manga_regex                     = ctre::search<"text/manga", ctre::case_insensitive>;
+        constexpr auto poll_regex                      = ctre::search<"text/poll", ctre::case_insensitive>;
+        constexpr auto bonus_regex                     = ctre::search<"text/bonus", ctre::case_insensitive>;
+        constexpr auto copyright_regex                 = ctre::search<"text/copyright", ctre::case_insensitive>;
+        constexpr auto ncx_regex                       = ctre::search<"dtbncx", ctre::case_insensitive>; // mediatype
+        constexpr auto stylesheet_regex                = ctre::search<"css", ctre::case_insensitive>; // mediatype
+        constexpr auto image_regex                     = ctre::search<"image/", ctre::case_insensitive>; // mediatype
+        constexpr auto toc_regex                       = ctre::search<"toc.xhtml", ctre::case_insensitive>;
+        constexpr auto characters_regex                = ctre::search<"text/character", ctre::case_insensitive>;
+        constexpr auto signup_regex                    = ctre::search<"text/signup", ctre::case_insensitive>;
+
+        if (image_regex(mediatype)) {
+            return IMAGE;
+        }
+        if (ncx_regex(mediatype)) {
+            return NCX;
+        }
+        if (stylesheet_regex(mediatype)) {
+            return STYLESHEET;
+        }
+        if (chapter_regex(href)) {
+            return CHAPTER;
+        }
+        if (cover_regex(href)) {
+            return COVER; // after chapter, so actually they are classified as chapter for now
+        }
+
+        if (volume::FB3 == vol) { // must be before other maps
+            if (map_ehrenfest_href_regex(href)) {
+                //return MAP_RA_LIBRARY;
+                return CHAPTER; // Treat the RA Library map as an insert image rather than a map
+            }
+        }
+
+        if (toc_label) {
+            if (map_ehrenfest_duchy_label_regex(*toc_label)) {
+                return MAP_EHRENFEST_DUCHY;
+            }
+            if (map_yurgenschmidt_label_regex(*toc_label)) {
+                return MAP_YURGENSCHMIDT;
+            }
+            if (map_ehrenfest_city_label_regex(*toc_label)) {
+                return MAP_EHRENFEST_CITY;
+            }
+        } else {
+            if (map_ehrenfest_href_regex(href)) {
+                return MAP_EHRENFEST_DUCHY;
+            }
+            if (map_yurgenschmidt_href_regex(href)) {
+                return MAP_YURGENSCHMIDT;
+            }
+        }
+
+        if (frontmatter_regex(href)) {
+            return FRONTMATTER; // must be after maps
+        }
+        if (family_tree_regex(href)) {
+            return FAMILY_TREE;
+        }
+        if (afterword_regex(href)) {
+            return AFTERWORD;
+        }
+        if (manga_regex(href)) {
+            return MANGA;
+        }
+        if (poll_regex(href)) {
+            return POLL;
+        }
+        if (bonus_regex(href)) {
+            return BONUS;
+        }
+        if (copyright_regex(href)) {
+            return COPYRIGHT;
+        }
+        if (toc_regex(href)) {
+            return TOC;
+        }
+        if (characters_regex(href)) {
+            return CHARACTERS; // after chapter, so actually they are classified as chapter for now
+        }
+        if (signup_regex(href)) {
+            return SIGNUP;
+        }
+
+        throw std::logic_error("Unable to identify chapter type");
+    }
+
+    template<typename StringHref, typename StringMedia, typename StringLabel>
+    constexpr volume_definition(volume vol, StringHref&& href, StringMedia&& mediatype, StringLabel&& toc_label, bool in_spine)
+        : vol(vol),
+          href(std::forward<StringHref>(href)),
+          mediatype(std::forward<StringMedia>(mediatype)),
+          toc_label(std::forward<StringLabel>(toc_label)),
+          in_spine(in_spine),
+          type(evaluate_chapter_type()) {}
 };
+
+
 
 template<std::ranges::input_range R>
 struct definition_view_t : public std::ranges::view_interface<definition_view_t<R>>
