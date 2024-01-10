a431f5a88867c5c89bec4cbae0919ce11cfa478b
Andrew Potter <agpotter@gmail.com>
2024-01-10 00:02:00 -0800

    Fix const correctness

--- src/main.cpp
+++ src/main.cpp
@@ -165,7 +165,7 @@ void do_dump()
 
 namespace {
     extern "C" {
-        void _xmlStructuredErrorFunc (void *, xmlErrorPtr error)
+        void _xmlStructuredErrorFunc (void *, const xmlErrorPtr error)
         {
             switch (error->level) {
                 case XML_ERR_NONE:
